030422c8f2bd354e5d7fe75a3307c2e6dcd20390
mva7873 <mva7873@stacmail.net>
2020-02-27 19:34:53 +1300

    traval plan with lists

--- traval plan.py
+++ traval plan.py
@@ -1,2 +1,7 @@
-accommodation  = []
-take_off = []
+#traval plan
+
+#lists
+
+accommodation  = [["Sydney", 120], ["Tonga", 40], ["Shanghai", 60], ["London", 80]]
+take_off = [["Auclkuland", 0], ["Wellington", 50], ["Christchurch", 75]]
+destination = [["Sydney", 326], ["Tonga", 378], ["Shanghai", 1436], ["London", 2376]]
