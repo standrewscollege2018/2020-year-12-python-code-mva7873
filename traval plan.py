8feb32ba4014e0db3290465bcc2ef1394a0d0843
mva7873 <mva7873@stacmail.net>
2020-03-02 12:31:38 +1300

    work on print lisy

--- traval plan.py
+++ traval plan.py
@@ -2,6 +2,10 @@
 
 #lists
 
-accommodation  = [["Sydney", 120], ["Tonga", 40], ["Shanghai", 60], ["London", 80]]
-take_off = [["Auclkuland", 0], ["Wellington", 50], ["Christchurch", 75]]
-destination = [["Sydney", 326], ["Tonga", 378], ["Shanghai", 1436], ["London", 2376]]
+accommodation  = [["Sydney","$", 120], ["Tonga","$", 40], ["Shanghai","$", 60], ["London","$", 80]]
+take_off = [["Auclkuland","$", 0], ["Wellington",'$', 50], ["Christchurch","$", 75]]
+destination = [["Sydney","$", 326], ["Tonga","$", 378], ["Shanghai","$", 1436], ["London","$", 2376]]
+acom_costs = (0)
+Traval_costs = (0)
+print("please enter teh city that you are departing from, as some of them may increase costs due to the needing to be conecting flights")
+print (take_off[1])
