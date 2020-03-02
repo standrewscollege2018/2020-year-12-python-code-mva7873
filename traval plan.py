176f111c1ab6c88014faff209f3032b99f72b2c9
mva7873 <mva7873@stacmail.net>
2020-03-03 09:34:46 +1300

    take off print done

--- traval plan.py
+++ traval plan.py
@@ -7,5 +7,16 @@ take_off = [["Auclkuland","$", 0], ["Wellington",'$', 50], ["Christchurch","$", 
 destination = [["Sydney","$", 326], ["Tonga","$", 378], ["Shanghai","$", 1436], ["London","$", 2376]]
 acom_costs = (0)
 Traval_costs = (0)
-print("please enter teh city that you are departing from, as some of them may increase costs due to the needing to be conecting flights")
-print (take_off[1])
+take_off_slection=true
+print("please enter the city that you are departing from, as some of them may increase costs due to the needing to be conecting flights")
+(print(take_off[0][0],end = ' '), print(take_off[0][1],end = ' '), print(take_off[0][2]))
+(print(take_off[1][0],end = ' '), print(take_off[1][1],end = ' '), print(take_off[1][2]))
+(print(take_off[2][0],end = ' '), print(take_off[2][1],end = ' '), print(take_off[2][2]))
+take_off_choice = input("enter the city you wish to leave from")
+while take_off_slection = true:
+elif take_off_choice = (take_off[0][0]):
+    Traval_costs + 0
+else print("invald input")
+
+
+take_off_slection=false
